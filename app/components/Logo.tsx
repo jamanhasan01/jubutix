341714019fda95c4fbb883313960a8d03d21b6a1
jaman hasan <jamanhasan246@gmail.com>
2025-07-30 04:49:31 +0600

    i have fix typescript error in up scroll button

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import favicon from '../../public/favicon.png'
 
 const Logo = () => (
   <div className='flex items-center text-primary'>
     <div>
-      <Image src='/favicon.png' width={40} height={40} alt='logo' />
+      <Image src={favicon} width={40} height={40} alt='Jubutix Logo' />
     </div>
     <span className='font-bold text-2xl text-primary'>JUBUTIX</span>
   </div>
